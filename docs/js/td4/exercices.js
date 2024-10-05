b0afb688515b68b6c5515f95cc5997475dd94666
Romain Q. <romain.qnm@gmail.com>
2024-10-05 14:07:58 +0200

    Exercice 2 - Add select

--- docs/js/td4/exercices.js
+++ docs/js/td4/exercices.js
@@ -2,3 +2,13 @@ document.getElementById('changeColorBtn').addEventListener('click', function () 
     document.body.style.backgroundColor = document.body.style.backgroundColor === 'purple' ? 'white' : 'purple';
   });
   
+  document.getElementById('addItemBtn').addEventListener('click', function () {
+    const itemValue = document.getElementById('itemInput').value;
+    if (itemValue) {
+      const li = document.createElement('li');
+      li.textContent = itemValue;
+      document.getElementById('itemList').appendChild(li);
+      document.getElementById('itemInput').value = '';
+    }
+  });
+  
