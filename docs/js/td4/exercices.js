4cc2cdf1e26043983cbf79fe50efd47491cfe93a
Romain Q. <romain.qnm@gmail.com>
2024-10-05 11:50:37 +0200

    Reprise du TD4 après corrections dans le main

---
+++ docs/js/td4/exercices.js
@@ -0,0 +1,9 @@
+const bt = (id)=>document.getElementById("id");
+
+if(bt("bgcolor")){
+    bt("bgcolor").addEventListener("click", ()=> ){
+        document.body.backgroundColor = "darkcyan"
+    };
+
+
+}
