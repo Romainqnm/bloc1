35bd97a1a63abdbb2aee2ef5c3fb1eb13de6793d
Romain Q. <romain.qnm@gmail.com>
2024-10-07 11:46:59 +0200

    Mentions Legales V4 - Requêtes ajax : redirige et ouvre la page en fonction du succès ou d'une potentielle erreur lors de l'ouverture

--- docs/js/td4/mentions-legales-4.js
+++ docs/js/td4/mentions-legales-4.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
 
         var url = $(this).attr('href');
 
+        $.ajax({
+            url: url,
+            success: function(response) {
+                $('#main-container').html($(response).find('body').html());
+            },
+            error: function() {
+                $('#main-container').html('<p>Erreur de chargement. Veuillez réessayer.</p>');
+            }
+        });
     });
 });
