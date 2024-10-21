98766fedf538a22eee46d5a4e9189f60996c672d
Romain Q. <romain.qnm@gmail.com>
2024-10-21 10:59:42 +0200

    TD5 - Message - Ajout de trois liens hypertextes pour afficher des messages prédéfinis via l'URL

--- docs/pages/td5/Message.php
+++ docs/pages/td5/Message.php
@@ -12,5 +12,9 @@
     $message = isset($_GET['message']) ? $_GET['message'] : "Message par défaut";
     echo "<p>$message</p>";
     ?>
+    
+    <a href="?message=Hello">Message 1</a>
+    <a href="?message=Bonjour">Message 2</a>
+    <a href="?message=Salut">Message 3</a>
 </body>
 </html>
