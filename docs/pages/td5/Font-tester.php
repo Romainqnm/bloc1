bb3e1f88c1503bd6150b30f6fe73860712122492
Romain Q. <romain.qnm@gmail.com>
2024-11-03 17:50:28 +0100

    Exercice PHP - Font Tester - Création du formulaire pour récupérer un message, sa taille, et sa couleur

--- docs/pages/td5/Font-tester.php
+++ docs/pages/td5/Font-tester.php
@@ -16,6 +16,19 @@
 <h1>Testeur de Polices</h1>
 
 <form method="GET" action="font-tester.php">
+    <label for="message">Message :</label>
+    <input type="text" name="message" id="message" value="<?php echo htmlspecialchars($message); ?>"><br>
+
+    <label for="size">Taille :</label>
+    <input type="number" name="size" id="size" value="<?php echo $size; ?>"><br>
+
+    <label for="color">Couleur :</label>
+    <input type="color" name="color" id="color" value="<?php echo $color; ?>"><br>
+
+    <input type="submit" value="Afficher le message">
+</form>
+
+<form method="GET" action="font-tester.php">
     <input type="hidden" name="message" value="<?php echo htmlspecialchars($message); ?>">
     <input type="hidden" name="color" value="<?php echo htmlspecialchars($color); ?>">
     <input type="hidden" name="size" value="<?php echo $size + 5; ?>">
