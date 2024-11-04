e6eb4c1023e812d13c42234204460f0ac16cad2b
Romain Q. <romain.qnm@gmail.com>
2024-11-04 10:21:57 +0100

    Exercice PHP - Lorem Ipsum Gen - Base HTML => Gère le formulaire d'affichage des paragraphes & la zone d'affichage de ces derniers

--- docs/pages/td5/Lorem-ipsum-gen.php
+++ docs/pages/td5/Lorem-ipsum-gen.php
@@ -7,5 +7,17 @@
 </head>
 <body>
 
+<h1>Générateur de Texte Lorem Ipsum</h1>
+
+
+<form method="POST" action="lorem-ipsum-gen.php">
+    <label for="count">Nombre de paragraphes :</label>
+    <input type="number" name="count" id="count" min="1" max="30" value="1">
+    <input type="submit" value="Générer">
+</form>
+
+<label for="result">Résultat :</label>
+<textarea id="result" rows="10" cols="70" readonly><?php echo htmlspecialchars($output); ?></textarea>
+
 </body>
 </html>
