a048e18b543a30650ecefe7f179189b09acc844d
Romain Q. <romain.qnm@gmail.com>
2024-11-04 09:56:53 +0100

    Exercice PHP - Font-tester-form - Utilisation de la méthode POST à l'aide du code HTML => Menu de sélection des styles

--- docs/pages/td5/Font-tester-form.php
+++ docs/pages/td5/Font-tester-form.php
@@ -13,6 +13,20 @@
 </head>
 <body>
 
+<h1>Testeur de Polices avec Formulaire</h1>
+
+<form method="POST" action="font-tester-form.php">
+    <label for="message">Message :</label><br>
+    <textarea name="message" id="message" rows="4" cols="50" placeholder="Entrez votre message ici..."><?php echo htmlspecialchars($message); ?></textarea><br>
+
+    <label for="size">Taille :</label>
+    <input type="number" name="size" id="size" value="<?php echo $size; ?>"><br>
+
+    <label for="color">Couleur :</label>
+    <input type="color" name="color" id="color" value="<?php echo $color; ?>"><br>
+
+    <input type="submit" value="Afficher le message">
+</form>
 
 </body>
 </html>
